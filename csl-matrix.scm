f2f57d91bdbe8ef3af66bcb531e46cc4cdbb53cb
dieggsy <dieggsy@protonmail.com>
2018-05-23 14:14:23 -0400

    Use complex matrices for csl:matrix, add matrix module

--- csl-matrix.scm
+++ csl-matrix.scm
@@ -1,145 +1,182 @@
-(define-record-type csl:matrix
-  (csl:ptr->matrix data)
-  csl:matrix?
-  (data csl:matrix->ptr))
+(module csl-matrix *
+  (import chicken scheme foreign foreigners data-structures)
+  (use numbers
+       csl-vector
+       fmt
+       srfi-13
+       ports)
 
-(define (csl:list->matrix lst)
-  (let* ((rows (length lst))
-         (cols (length (car lst)))
-         (m (gsl_matrix_alloc_gc rows cols)))
-    (let loop1 ((i 0)
-                (l1 lst))
-      (if (= i rows)
-          (csl:ptr->matrix m)
-          (begin
-            (let loop2 ((j 0)
-                        (l2 (car l1)))
-              (gsl_matrix_set m i j (car l2))
-              (if (= j (- cols 1))
-                  (void)
-                  (loop2 (+ j 1) (cdr l2))))
-            (loop1 (+ i 1) (cdr l1)))))))
+  (include "gsl-block.scm")
+  (include "gsl-matrix-complex.scm")
 
-(define (csl:matrix->list m)
-  (let* ((data (csl:matrix->ptr m))
-         (shape (csl:matrix-shape m))
-         (rows (car shape))
-         (columns (cdr shape)))
-    (let loop ((i (- rows 1))
-               (res '()))
-      (if (= i -1)
-          res
-          (loop
-           (- i 1)
-           (cons
-            (let loop ((j (- columns 1))
-                       (res '()))
-              (if (= j -1)
-                  res
-                  (loop (- j 1) (cons (gsl_matrix_get data i j) res))))
-            res))))))
+  (define-record-type csl:matrix
+    (csl:ptr->matrix data)
+    csl:matrix?
+    (data csl:matrix->ptr))
 
-(define (csl:matrix . args)
-  (csl:list->matrix args))
+  (define (csl:list->matrix lst)
+    (let* ((rows (length lst))
+           (cols (length (car lst)))
+           (m (gsl_matrix_complex_alloc_gc rows cols)))
+      (let loop1 ((i 0)
+                  (l1 lst))
+        (if (= i rows)
+            (csl:ptr->matrix m)
+            (begin
+              (let loop2 ((j 0)
+                          (l2 (car l1)))
+                (gsl_matrix_complex_set m i j (car l2))
+                (if (= j (- cols 1))
+                    (void)
+                    (loop2 (+ j 1) (cdr l2))))
+              (loop1 (+ i 1) (cdr l1)))))))
 
-(define (csl:make-matrix n1 n2 #!optional fill)
-  (let ((m (gsl_matrix_alloc_gc n1 n2)))
-    (when fill
-      (gsl_matrix_set_all m fill))
-    (csl:ptr->matrix m)))
+  (define (csl:matrix->list m)
+    (let* ((data (csl:matrix->ptr m))
+           (shape (csl:matrix-shape m))
+           (rows (car shape))
+           (columns (cdr shape)))
+      (let loop ((i (- rows 1))
+                 (res '()))
+        (if (= i -1)
+            res
+            (loop
+             (- i 1)
+             (cons
+              (let loop ((j (- columns 1))
+                         (res '()))
+                (if (= j -1)
+                    res
+                    (loop (- j 1) (cons (gsl_matrix_complex_get data i j) res))))
+              res))))))
 
-(define (csl:matrix-shape m)
-  (cons
-   (gsl_matrix.size1 (csl:matrix->ptr m))
-   (gsl_matrix.size2 (csl:matrix->ptr m))))
+  (define (csl:matrix . args)
+    (csl:list->matrix args))
 
-(define (csl:vector-map! f . m))
+  (define (csl:make-matrix n1 n2 #!optional fill)
+    (let ((m (gsl_matrix_complex_alloc_gc n1 n2)))
+      (when fill
+        (gsl_matrix_complex_set_all m fill))
+      (csl:ptr->matrix m)))
 
-(define (csl:matrix-map f . m)
-  (let* ((rows (apply min (map (o car csl:matrix-shape) m)))
-         (cols (apply min (map (o cdr csl:matrix-shape) m)))
-         (d (map csl:matrix->ptr m))
-         (r (gsl_matrix_alloc_gc rows cols)))
-    (let loop ((i (- rows 1)))
-      (if (= i -1)
-          (csl:ptr->matrix r)
-          (begin
-            (let loop ((j (- cols 1)))
-              (if (= j -1)
-                  #f
-                  (begin
-                    (gsl_matrix_set r i j
-                                    (apply f
-                                           (map (cut gsl_matrix_get <> i j) d)))
-                    (loop (- j 1)))))
-            (loop (- i 1)))))))
+  (define (csl:matrix-shape m)
+    (cons
+     (gsl_matrix_complex.size1 (csl:matrix->ptr m))
+     (gsl_matrix_complex.size2 (csl:matrix->ptr m))))
 
-;; (define (csl:matrix-ref))
+  (define (csl:matrix-map f . m)
+    (let* ((rows (apply min (map (o car csl:matrix-shape) m)))
+           (cols (apply min (map (o cdr csl:matrix-shape) m)))
+           (d (map csl:matrix->ptr m))
+           (r (gsl_matrix_complex_alloc_gc rows cols)))
+      (let loop ((i (- rows 1)))
+        (if (= i -1)
+            (csl:ptr->matrix r)
+            (begin
+              (let loop ((j (- cols 1)))
+                (if (= j -1)
+                    #f
+                    (begin
+                      (gsl_matrix_complex_set r i j
+                                              (apply f
+                                                     (map (cut gsl_matrix_complex_get <> i j) d)))
+                      (loop (- j 1)))))
+              (loop (- i 1)))))))
 
-;; (define (csl:matrix-set!))
+  (define (csl:matrix-map! f . m)
+    (let* ((rows (apply min (map (o car csl:matrix-shape) m)))
+           (cols (apply min (map (o cdr csl:matrix-shape) m)))
+           (d (map csl:matrix->ptr m)))
+      (let loop ((i (- rows 1)))
+        (if (= i -1)
+            (void)
+            (begin
+              (let loop ((j (- cols 1)))
+                (if (= j -1)
+                    #f
+                    (begin
+                      (gsl_matrix_complex_set (car d) i j
+                                              (apply f
+                                                     (map (cut gsl_matrix_complex_get <> i j) d)))
+                      (loop (- j 1)))))
+              (loop (- i 1)))))))
 
-(define (csl:matrix-fill! m n)
-  (gsl_matrix_set_all (csl:matrix->ptr m) n))
+  ;; (define (csl:matrix-ref))
 
-(define (csl:identity-matrix n)
-  (let ((m (gsl_matrix_alloc_gc n n)))
-    (gsl_matrix_set_identity m)
-    (csl:ptr->matrix m)))
+  ;; (define (csl:matrix-set!))
 
-(define (csl:matrix-transpose m)
-  (let* ((d (csl:matrix->ptr m))
-         (shape (csl:matrix-shape m))
-         (rows (car shape))
-         (cols (cdr shape))
-         (r (gsl_matrix_alloc_gc cols rows)))
-    (gsl_matrix_transpose_memcpy r d)
-    (csl:ptr->matrix r)))
+  (define (csl:matrix-fill! m n)
+    (gsl_matrix_complex_set_all (csl:matrix->ptr m) n))
 
-(define (csl:matrix-row m n)
-  (csl:ptr->vector
-   (gsl_matrix_row (csl:matrix->ptr m) n)))
+  (define (csl:identity-matrix n)
+    (let ((m (gsl_matrix_complex_alloc_gc n n)))
+      (gsl_matrix_complex_set_identity m)
+      (csl:ptr->matrix m)))
 
-(define (csl:matrx-column m n)
-  (csl:ptr->vector
-   (gsl_matrix_column (csl:matrix->ptr m) n)))
+  (define (csl:matrix-copy m)
+    (let* ((shape (csl:matrix-shape m))
+           (c (gsl_matrix_complex_alloc_gc (car shape) (cdr shape))))
+      (gsl_matrix_complex_memcpy c (csl:matrix->ptr m))
+      (csl:ptr->matrix c)))
 
-(define (csl:matrix-print m out)
-  (let* ((cols (csl:matrix->list
-                (csl:matrix-transpose m)))
-         (str-cols (map (lambda (x)
-                          (string-join
-                           (map number->string x)
-                           "\n"))
-                        cols))
-         (fmt-cols (apply append
-                          (map (lambda (x)
-                                 (list 'right (dsp x) " "))
-                               str-cols)))
-         (fmt-str (call-with-output-string
-                    (lambda (strout)
-                      (fmt strout (apply tabular fmt-cols))))))
-    (fprintf out "#,(csl:matrix(~a))"
-             (string-join
-              (intersperse (map (cut string-drop-right <> 1)
-                                (string-split fmt-str "\n"))
-                           (format ")\n~a("
-                                   (make-string 13 #\space)))
-              ""))))
-;; ;; (define (matrix-print m out)
-;; ;;   (let* ((cols ))))
+  (define (csl:matrix-row-ref m n)
+    (let* ((cols (cdr (csl:matrix-shape m)))
+           (v (gsl_vector_complex_alloc_gc cols)))
+      (gsl_matrix_complex_get_row v (csl:matrix->ptr m) n)
+      (csl:ptr->vector v)))
 
-(define-reader-ctor 'csl:matrix csl:matrix)
+  (define (csl:matrix-transpose m)
+    (let* ((d (csl:matrix->ptr m))
+           (shape (csl:matrix-shape m))
+           (rows (car shape))
+           (cols (cdr shape))
+           (r (gsl_matrix_complex_alloc_gc cols rows)))
+      (gsl_matrix_complex_transpose_memcpy r d)
+      (csl:ptr->matrix r)))
 
-(define-record-printer (csl:matrix m out)
-  (let* ((shape (csl:matrix-shape m))
-         (rows (car shape))
-         (columns (cdr shape)))
-    (if (or (> rows 20)
-            (> columns 20))
-        (fprintf out "#<~ax~a csl:matrix>" rows columns)
-        (##sys#with-print-length-limit
-         +inf.0
-         (lambda ()
-           (csl:matrix-print m out))))))
+  (define (csl:matrix-row m n)
+    (csl:ptr->vector
+     (gsl_matrix_complex_row (csl:matrix->ptr m) n)))
 
-;; Operations
+  (define (csl:matrx-column m n)
+    (csl:ptr->vector
+     (gsl_matrix_complex_column (csl:matrix->ptr m) n)))
+
+  (define (csl:matrix-print m out)
+    (let* ((cols (csl:matrix->list
+                  (csl:matrix-transpose m)))
+           (str-cols (map (lambda (x)
+                            (string-join
+                             (map number->string x)
+                             "\n"))
+                          cols))
+           (fmt-cols (apply append
+                            (map (lambda (x)
+                                   (list 'right (dsp x) " "))
+                                 str-cols)))
+           (fmt-str (call-with-output-string
+                      (lambda (strout)
+                        (fmt strout (apply tabular fmt-cols))))))
+      (fprintf out "#,(csl:matrix(~a))"
+               (string-join
+                (intersperse (map (cut string-drop-right <> 1)
+                                  (string-split fmt-str "\n"))
+                             (format ")\n~a("
+                                     (make-string 13 #\space)))
+                ""))))
+  ;; ;; (define (matrix-print m out)
+  ;; ;;   (let* ((cols ))))
+
+  (define-reader-ctor 'csl:matrix csl:matrix)
+
+  (define-record-printer (csl:matrix m out)
+    (let* ((shape (csl:matrix-shape m))
+           (rows (car shape))
+           (columns (cdr shape)))
+      (if (or (> rows 20)
+              (> columns 20))
+          (fprintf out "#<~ax~a csl:matrix>" rows columns)
+          (##sys#with-print-length-limit
+           +inf.0
+           (lambda ()
+             (csl:matrix-print m out)))))))
---
+++ gsl-matrix-complex.scm
@@ -0,0 +1,220 @@
+(foreign-declare "#include <gsl/gsl_matrix.h>")
+(foreign-declare "#include <gsl/gsl_complex_math.h>")
+
+(define-foreign-record-type (gsl_vector_complex "gsl_vector_complex")
+  (unsigned-int size gsl_vector_complex.size)
+  (unsigned-int stride gsl_vector_complex.stride)
+  ((c-pointer double) data gsl_vector_complex.data)
+  (gsl_block_complex block gsl_vector_complex.block)
+  (int owner gsl_vector_complex.owner))
+
+(define-foreign-record-type (gsl_matrix_complex "gsl_matrix_complex")
+  (unsigned-int size1 gsl_matrix_complex.size1)
+  (unsigned-int size2 gsl_matrix_complex.size2)
+  (unsigned-int tda gsl_matrix_complex.tda)
+  ((c-pointer double) data gsl_matrix_complex.data)
+  (gsl_block_complex block gsl_matrix_complex.block)
+  (int owner gsl_matrix_complex.owner))
+
+(define-external (numbers_make_rect (double r) (double i)) scheme-object
+  (make-rectangular r i))
+
+;; ;; Matrix allocation
+(define gsl_matrix_complex_free
+  (foreign-safe-lambda void "gsl_matrix_complex_free" gsl_matrix_complex))
+
+(define gsl_matrix_complex_alloc
+  (foreign-safe-lambda gsl_matrix_complex "gsl_matrix_complex_alloc" unsigned-int unsigned-int))
+(define (gsl_matrix_complex_alloc_gc n1 n2)
+  (set-finalizer! (gsl_matrix_complex_alloc n1 n2) gsl_matrix_complex_free))
+
+(define gsl_matrix_complex_calloc
+  (foreign-safe-lambda gsl_matrix_complex "gsl_matrix_complex_calloc" unsigned-int unsigned-int))
+(define (gsl_matrix_complex_calloc_gc n1 n2)
+  (set-finalizer! (gsl_matrix_complex_calloc n1 n2) gsl_matrix_complex_free))
+
+;; ;; Accessing matrix elements
+(define gsl_matrix_complex_get
+  (foreign-safe-lambda* scheme-object (((const gsl_matrix_complex) m)
+                                       ((const unsigned-int) i)
+                                       ((const unsigned-int) j))
+    "gsl_complex zout = gsl_matrix_complex_get(m,i,j);"
+    "C_return(numbers_make_rect(GSL_REAL(zout),GSL_IMAG(zout)));"))
+
+(define (gsl_matrix_complex_set m i j z)
+  (let ((rz (real-part z))
+        (iz (imag-part z)))
+    ((foreign-safe-lambda* void ((gsl_matrix_complex m)
+                                 ((const unsigned-int) i)
+                                 ((const unsigned-int) j)
+                                 (double rz) (double iz))
+       "gsl_complex zset = gsl_complex_rect(rz,iz);"
+       "gsl_matrix_complex_set(m,i,j,zset);")
+     m i j rz iz)))
+
+(define gsl_matrix_complex_ptr
+  (foreign-safe-lambda (c-pointer double) "gsl_matrix_complex_ptr"
+    gsl_matrix_complex
+    unsigned-int
+    unsigned-int))
+
+(define gsl_matrix_complex_const_ptr
+  (foreign-safe-lambda (c-pointer double) "gsl_matrix_complex_ptr"
+    gsl_matrix_complex
+    unsigned-int
+    unsigned-int))
+
+;; ;; Initializing matrix elments
+(define (gsl_matrix_complex_set_all m z)
+  (let ((rz (real-part z))
+        (iz (imag-part z)))
+    ((foreign-safe-lambda* void ((gsl_matrix_complex m)
+                                 (double rz) (double iz))
+       "gsl_complex zset = gsl_complex_rect(rz,iz);"
+       "gsl_matrix_complex_set_all(m,zset);")
+     m rz iz)))
+
+(define gsl_matrix_complex_set_zero
+  (foreign-safe-lambda void "gsl_matrix_complex_set_zero" gsl_matrix_complex))
+
+(define gsl_matrix_complex_set_identity
+  (foreign-safe-lambda void "gsl_matrix_complex_set_identity" gsl_matrix_complex))
+
+(define-syntax define-gsl-matrix-subview-binding
+  (er-macro-transformer
+   (lambda (form rename compare)
+     (let ((type (conc "gsl_" (fourth form))))
+       `(define ,(string->symbol (conc "gsl_" (second form) "_" (third form)))
+          (foreign-safe-lambda*
+              ,(string->symbol (conc "gsl_" (fourth form)))
+              ,(fifth form)
+            ,(conc type " *p0 = malloc(sizeof(" type "));")
+            ,(conc "gsl_" (fourth form) "_view p1 = gsl_" (second form) "_" (third form) "("
+                   (string-join (map (lambda (a) (symbol->string (second a))) (fifth form)) ",")
+                   ");")
+            ,(conc "memcpy(p0, &p1."
+                   (case (fourth form)
+                     ((vector_complex)
+                      "vector")
+                     ((matrix_complex)
+                      "matrix"))
+                   ", sizeof(" (conc "gsl_" (fourth form)) "));")
+            "C_return(p0);"))))))
+
+;; ;; Matrix views
+(define-gsl-matrix-subview-binding matrix_complex submatrix matrix_complex
+  ((gsl_matrix_complex m)
+   (unsigned-int k1)
+   (unsigned-int k2)
+   (unsigned-int n1)
+   (unsigned-int n2)))
+
+(define-gsl-matrix-subview-binding matrix_complex view_vector matrix_complex
+  ((c-pointer v) (unsigned-int n1) (unsigned-int n2)))
+
+;; (define-gsl-matrix-subview-binding matrix view_vector_with_tda matrix
+;;   ((c-pointer v) (unsigned-int n1) (unsigned-int n2) (unsigned-int tda)))
+
+;; ;; Creating row and column views
+(define-gsl-matrix-subview-binding matrix_complex row vector_complex
+  ((c-pointer m) (unsigned-int i)))
+(define-gsl-matrix-subview-binding matrix_complex column vector_complex
+  ((c-pointer m) (unsigned-int j)))
+(define-gsl-matrix-subview-binding matrix_complex subrow vector_complex
+  ((c-pointer m) (unsigned-int i) (unsigned-int offset) (unsigned-int n)))
+(define-gsl-matrix-subview-binding matrix_complex subcolumn vector_complex
+  ((c-pointer m) (unsigned-int j) (unsigned-int offset) (unsigned-int n)))
+(define-gsl-matrix-subview-binding matrix_complex diagonal vector_complex
+  ((c-pointer m)))
+(define-gsl-matrix-subview-binding matrix_complex subdiagonal vector_complex
+  ((c-pointer m) (unsigned-int k)))
+(define-gsl-matrix-subview-binding matrix_complex superdiagonal vector_complex
+  ((c-pointer m) (unsigned-int k)))
+
+;; Copying matrices
+(define gsl_matrix_complex_memcpy
+  (foreign-safe-lambda int "gsl_matrix_complex_memcpy" gsl_matrix_complex gsl_matrix_complex))
+(define gsl_matrix_complex_swap
+  (foreign-safe-lambda int "gsl_matrix_complex_swap" gsl_matrix_complex gsl_matrix_complex))
+
+;; Copying rows and columns
+(define gsl_matrix_complex_get_row
+  (foreign-safe-lambda int "gsl_matrix_complex_get_row" gsl_vector_complex gsl_matrix_complex unsigned-int))
+
+(define gsl_matrix_complex_get_col
+  (foreign-safe-lambda int "gsl_matrix_complex_get_col" gsl_vector_complex gsl_matrix_complex unsigned-int))
+
+(define gsl_matrix_complex_set_row
+  (foreign-safe-lambda int "gsl_matrix_complex_set_row" gsl_matrix_complex unsigned-int gsl_vector_complex))
+
+(define gsl_matrix_complex_set_col
+  (foreign-safe-lambda int "gsl_matrix_complex_set_col" gsl_matrix_complex unsigned-int gsl_vector_complex))
+
+;; Exchanging rows and columns
+(define gsl_matrix_complex_swap_rows
+  (foreign-safe-lambda int "gsl_matrix_complex_swap_rows"
+    gsl_matrix_complex unsigned-int unsigned-int))
+
+(define gsl_matrix_complex_swap_columns
+  (foreign-safe-lambda int "gsl_matrix_complex_swap_columns"
+    gsl_matrix_complex unsigned-int unsigned-int))
+
+(define gsl_matrix_complex_swap_rowcol
+  (foreign-safe-lambda int "gsl_matrix_complex_swap_rowcol"
+    gsl_matrix_complex unsigned-int unsigned-int))
+
+(define gsl_matrix_complex_transpose_memcpy
+  (foreign-safe-lambda int "gsl_matrix_complex_transpose_memcpy" gsl_matrix_complex gsl_matrix_complex))
+
+(define gsl_matrix_complex_transpose
+  (foreign-safe-lambda int "gsl_matrix_complex_transpose" gsl_matrix_complex))
+
+;; Matrix operations
+(define gsl_matrix_complex_add
+  (foreign-safe-lambda int "gsl_matrix_complex_add" gsl_matrix_complex gsl_matrix_complex))
+
+(define gsl_matrix_complex_sub
+  (foreign-safe-lambda int "gsl_matrix_complex_sub" gsl_matrix_complex gsl_matrix_complex))
+
+(define gsl_matrix_complex_mul_elements
+  (foreign-safe-lambda int "gsl_matrix_complex_mul_elements" gsl_matrix_complex gsl_matrix_complex))
+
+(define gsl_matrix_complex_div_elements
+  (foreign-safe-lambda int "gsl_matrix_complex_div_elements" gsl_matrix_complex gsl_matrix_complex))
+
+(define (gsl_matrix_complex_scale m z)
+  (let ((rz (real-part z))
+        (iz (imag-part z)))
+    ((foreign-safe-lambda* int ((gsl_matrix_complex m)
+                                (double rz) (double iz))
+       "gsl_complex zin = gsl_complex_rect(rz,iz);"
+       "gsl_matrix_complex_scale(m,zin);")
+     m rz iz)))
+
+(define (gsl_matrix_complex_add_constant m z)
+  (let ((rz (real-part z))
+        (iz (imag-part z)))
+    ((foreign-safe-lambda* int ((gsl_matrix_complex m)
+                                (double rz) (double iz))
+       "gsl_complex zin = gsl_complex_rect(rz,iz);"
+       "gsl_matrix_complex_add_constant(m,zin);")
+     m rz iz)))
+
+;; Finding max and min elements of matrices
+;; (define gsl_matrix_complex_max (foreign-safe-lambda int "gsl_matrix_complex_max" gsl_matrix_complex))
+;; (define gsl_matrix_complex_min (foreign-safe-lambda int "gsl_matrix_complex_min" gsl_matrix_complex))
+;; ;; TODO (define matrix-minmax)
+;; (define gsl_matrix_complex_max_index
+;;   (foreign-safe-lambda void "gsl_matrix_complex_max_index"
+;;     gsl_matrix_complex (c-pointer unsigned-long) (c-pointer unsigned-long)))
+;; (define gsl_matrix_complex_min_index
+;;   (foreign-safe-lambda void "gsl_matrix_complex_min_index"
+;;     gsl_matrix_complex (c-pointer unsigned-long) (c-pointer unsigned-long)))
+;; TODO (define matrix-argminmax)
+
+;; Matrix properties
+(define gsl_matrix_complex_isnull (foreign-safe-lambda bool "gsl_matrix_complex_isnull" gsl_matrix_complex))
+(define gsl_matrix_complex_ispos (foreign-safe-lambda bool "gsl_matrix_complex_ispos" gsl_matrix_complex))
+(define gsl_matrix_complex_isneg (foreign-safe-lambda bool "gsl_matrix_complex_isneg" gsl_matrix_complex))
+(define gsl_matrix_complex_isnonneg (foreign-safe-lambda bool "gsl_matrix_complex_isnonneg" gsl_matrix_complex))
+(define gsl_matrix_complex_equal (foreign-safe-lambda bool "gsl_matrix_complex_equal" gsl_matrix_complex gsl_matrix_complex))
