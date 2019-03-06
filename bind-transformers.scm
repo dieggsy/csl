e95ffd712e43176d62591d7720874428b5f07154
dieggsy <dieggsy@protonmail.com>
2019-03-06 13:38:45 -0500

    Handle complex floats

--- bind-transformers.scm
+++ bind-transformers.scm
@@ -5,11 +5,19 @@
         (only chicken.syntax begin-for-syntax)
         bind)
 
-(foreign-declare "#include <gsl/gsl_complex_math.h>")
+(foreign-declare "#include <gsl/gsl_complex.h>")
 
 (foreign-declare "
 gsl_complex f64_to_complex(double *arg) {
-    return gsl_complex_rect(arg[0],arg[1]);
+    gsl_complex z;
+    GSL_SET_COMPLEX(&z,arg[0],arg[1]);
+    return z;
+}
+
+gsl_complex_float f32_to_complex(float *arg) {
+    gsl_complex_float z;
+    GSL_SET_COMPLEX(&z,arg[0],arg[1]);
+    return z;
 }
 " )
 
@@ -34,7 +42,7 @@ gsl_complex f64_to_complex(double *arg) {
   ;; and make it return a 2-element f64vector instead.
   (define (gsl-ret-transformer* x rename)
 
-    (define (make-complex-ret-lambda fl args body vec make-vec vec->list )
+    (define (make-complex-ret-lambda fl args body typestr vec make-vec vec->list )
       (let* ((argnames (map cadr args))
              ;; return-type -> void, add f64vector destination
              ;; argument, and cast to gsl-complex.
@@ -44,7 +52,7 @@ gsl_complex f64_to_complex(double *arg) {
                  void                           ;; new return type
                  ,(cons `(,vec dest) args) ;; add destination arg
                  (stmt
-                  (= "gsl_complex _z" ,body) ;; allocate, cast & assign
+                  (= ,(format "~a _z" typestr) ,body) ;; allocate, cast & assign
                   (= "dest[0]" "GSL_REAL(_z)")
                   (= "dest[1]" "GSL_IMAG(_z)")
                   ))
@@ -63,10 +71,10 @@ gsl_complex f64_to_complex(double *arg) {
       ;; return-type is a gsl-complex, need to convert
       ((foreign-lambda* ('struct (? (cut irregex-search "^gsl_complex" <>) type)) args body)
        (cond ((string=? type "gsl_complex")
-              (make-complex-ret-lambda foreign-lambda* args body
+              (make-complex-ret-lambda foreign-lambda* args body type
                                        'f64vector 'make-f64vector 'f64vector->list))
              ((string=? type "gsl_complex_float")
-              (make-complex-ret-lambda foreign-lambda* args body
+              (make-complex-ret-lambda foreign-lambda* args body type
                                        'f32vector 'make-f32vector 'f32vector->list))
              (else (error "Unknown complex type" type))))
       ((foreign-lambda* ('struct (? (cut irregex-match "gsl_vector(_\\w+)?_view" <>) type)) args body)
@@ -102,14 +110,13 @@ gsl_complex f64_to_complex(double *arg) {
       (else (bind-foreign-lambda* x rename))))
 
   (define (foo#gsl-arg-transformer* x rename)
-    (define (complex-type? type)
-      (and (pair? type)
-           (eq? (car type) 'struct)
-           (irregex-search "^gsl_complex" (cadr type))))
-    (match x
-      ;; return-type is a gsl-complex, need to convert
-      ((foreign-lambda* rtype (? (lambda (x) (any complex-type? (map car x))) args) body)
-       (when (debug)
+    (define (complex-type? str)
+      (lambda (type)
+       (and (pair? type)
+            (eq? (car type) 'struct)
+            (string=? (cadr type) str))))
+    (define (make-complex-arg-lambda fl rtype args body typestr convertstr vec)
+      (when (debug)
          (print "----LAMBDA:")
          (pp x)
          (print "=>"))
@@ -119,10 +126,10 @@ gsl_complex f64_to_complex(double *arg) {
                   (and (eq? (cadr spec) varname)
                        (car spec))) args))
          (define (gsl-complex? type)
-           (and (equal? type '(struct "gsl_complex"))))
+           (and (equal? type `(struct ,typestr))))
          (define (gsl-complex->f64vector as)
            (if (gsl-complex? (car as))
-               (list 'f64vector (cadr as))
+               (list vec (cadr as))
                as))
          ;; recursively look for variables which reference arguments of
          ;; type struct and cast from f64vector to struct gsl-complex*.
@@ -130,24 +137,30 @@ gsl_complex f64_to_complex(double *arg) {
            (if (list? body)
                (map dereference body)
                (if (and (symbol? body) (gsl-complex? (type body)))
-                   (conc "f64_to_complex(" body ")")
+                   (conc convertstr "(" body ")")
                    body)))
          (let ((final-lambda
                 `(lambda ,argnames
                    (,(gsl-ret-transformer*
-                      `(,foreign-lambda* ,rtype
+                      `(,fl ,rtype
                            ,(map gsl-complex->f64vector args)
                          ,(dereference body))
                       rename)
                     ,@(map (lambda (x)
                              (if (gsl-complex? (type x))
-                                 `(f64vector (exact->inexact (real-part ,x))
+                                 `(,vec (exact->inexact (real-part ,x))
                                              (exact->inexact (imag-part ,x)))
                                  x))
                            argnames)))))
            (when (debug)
              (pp final-lambda))
            final-lambda)))
+    (match x
+      ;; return-type is a gsl-complex, need to convert
+      ((foreign-lambda* rtype (? (lambda (x) (any (complex-type? "gsl_complex") (map car x))) args) body)
+       (make-complex-arg-lambda foreign-lambda* rtype args body "gsl_complex" "f64_to_complex" 'f64vector))
+      ((foreign-lambda* rtype (? (lambda (x) (any (complex-type? "gsl_complex_float") (map car x))) args) body)
+       (make-complex-arg-lambda foreign-lambda* rtype args body "gsl_complex_float" "f32_to_complex" 'f32vector))
       (else
        (gsl-ret-transformer*
         x
