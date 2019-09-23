6f096e3cd8485adc3106d5db5806e90141af303a
dieggsy <dieggsy@protonmail.com>
2019-09-23 02:06:03 -0400

    Remove bind dependency from gsl.permutation

--- permutation/gsl.permutation.scm
+++ permutation/gsl.permutation.scm
@@ -10,88 +10,114 @@
                          permutation-get
                          permutation-swap!
                          permutation-size
-                         permutation-valid?
+                         permutation-valid
                          permutation-reverse!
                          permutation-inverse!
                          permutation-next!
                          permutation-prev!
                          permutation-mul!
+                         permutation-fwrite
+                         permutation-fread!
+                         permutation-fprintf
+                         permutation-fscanf!
                          permutation-linear-to-canonical!
                          permutation-canonical-to-linear!
                          permutation-inversions
                          permutation-linear-cycles
                          permutation-canonical-cycles)
   (import scheme
-          bind
           chicken.foreign
-          (only chicken.base include define-record-type)
-          (only chicken.gc set-finalizer!))
+          (only chicken.base include define-record-type))
 
-  (include "csl-error.scm")
-  (include "bind-transformers.scm")
+  (include "utils/error-handler.scm")
+  (include "utils/stdio.scm")
 
   (foreign-declare "#include <gsl/gsl_permutation.h>")
-
-  (bind-options default-renaming: "")
-
-  (bind-rename/pattern "^gsl-" "")
 
   (define-record-type permutation
     (ptr->permutation ptr)
     permutation?
     (ptr permutation->ptr))
 
-  (bind-type
-   csl_permutation
-   (c-pointer "gsl_permutation")
-   permutation->ptr
-   ptr->permutation)
+  (define-foreign-type gsl-permutation
+    (nonnull-c-pointer "gsl_permutation")
+    permutation->ptr
+    ptr->permutation)
 
   ;;; Permutation allocation
-  (bind-rename/pattern
-   "(init|free|memcpy|swap|reverse|inverse|next|prev|mul|linear|canonical)$"
-   "\\1!")
+  (define permutation-alloc
+    (foreign-lambda gsl-permutation "gsl_permutation_alloc" size_t))
+  (define permutation-calloc
+    (foreign-lambda gsl-permutation "gsl_permutation_calloc" size_t))
 
-  (bind-rename "gsl_permutation_alloc" "%permutation-alloc")
-  (bind-rename "gsl_permutation_calloc" "%permutation-calloc")
-  (bind "csl_permutation gsl_permutation_alloc(size_t)")
-  (bind "csl_permutation gsl_permutation_calloc(size_t)")
-
-  (define (permutation-alloc n)
-    (set-finalizer! (%permutation-alloc n) permutation-free!))
-
-  (define (permutation-calloc n)
-    (set-finalizer! (%permutation-calloc n) permutation-free!))
-
-  (bind "void gsl_permutation_init(csl_permutation)")
-  (bind "void gsl_permutation_free(csl_permutation)")
-  (bind "___safe void gsl_permutation_memcpy(csl_permutation, csl_permutation)")
-
-  (bind "size_t gsl_permutation_size(csl_permutation)")
+  (define permutation-init!
+    (foreign-lambda void "gsl_permutation_init" gsl-permutation))
+  (define permutation-free!
+    (foreign-lambda void "gsl_permutation_free" gsl-permutation))
+  (define permutation-memcpy!
+    (foreign-safe-lambda void "gsl_permutation_memcpy" gsl-permutation gsl-permutation))
 
   ;;; Accessing permutation elements
-  (bind "___safe size_t gsl_permutation_get(csl_permutation, size_t)")
-  (bind "___safe size_t gsl_permutation_swap(csl_permutation, size_t, size_t)")
+  (define permutation-get
+    (foreign-safe-lambda size_t "gsl_permutation_get" gsl-permutation size_t))
+  (define permutation-swap!
+    (foreign-safe-lambda gsl-errno "gsl_permutation_swap" gsl-permutation size_t size_t))
 
   ;;; Permutation properties
-  (bind "size_t gsl_permutation_size(csl_permutation)")
-  (bind-rename "gsl_permutation_valid" "permutation-valid?")
-  (bind "___safe int gsl_permutation_valid(csl_permutation)")
+  (define permutation-size
+    (foreign-lambda size_t "gsl_permutation_size" gsl-permutation))
+  (define permutation-valid
+    (foreign-safe-lambda gsl-errno "gsl_permutation_valid" gsl-permutation))
 
   ;;; Permutation functions
-  (bind "void gsl_permutation_reverse(csl_permutation)")
-  (bind "___safe int gsl_permutation_inverse(csl_permutation, csl_permutation)")
+  (define permutation-reverse!
+    (foreign-lambda void "gsl_permutation_reverse" gsl-permutation))
+  (define permutation-inverse!
+    (foreign-safe-lambda gsl-errno "gsl_permutation_inverse" gsl-permutation gsl-permutation))
 
-  (bind "int gsl_permutation_next(csl_permutation)")
-  (bind "int gsl_permutation_prev(csl_permutation)")
+  (define permutation-next!
+    (foreign-safe-lambda int "gsl_permutation_next" gsl-permutation))
+  (define permutation-prev!
+    (foreign-safe-lambda int "gsl_permutation_prev" gsl-permutation))
 
-  ;; Applying permutations
-  (bind "___safe int gsl_permutation_mul(csl_permutation, csl_permutation, csl_permutation);")
+  ;;; Applying permutations
+  (define permutation-mul!
+    (foreign-safe-lambda gsl-errno "gsl_permutation_mul" gsl-permutation gsl-permutation gsl-permutation))
 
-  ;; Permutations in cyclic form
-  (bind "___safe int gsl_permutation_linear_to_canonical (gsl_permutation * q, csl_permutation p);")
-  (bind "___safe int gsl_permutation_canonical_to_linear (gsl_permutation * p, csl_permutation q);")
+  ;;; Reading and writing permutations
+  (define (permutation-fwrite fileport permutation)
+    (let* ((FILE (get-c-file 'permutation-fwrite fileport)))
+      ((foreign-safe-lambda gsl-errno "gsl_permutation_fwrite"
+         (c-pointer "FILE") gsl-permutation)
+       FILE permutation)))
 
-  (bind "size_t gsl_permutation_inversions (csl_permutation p);")
-  (bind "size_t gsl_permutation_linear_cycles (csl_permutation p);")
-  (bind "size_t gsl_permutation_canonical_cycles (csl_permutation q);"))
+  (define (permutation-fread! fileport permutation)
+    (let* ((FILE (get-c-file 'permutation-fread fileport)))
+      ((foreign-safe-lambda gsl-errno "gsl_permutation_fread"
+         (c-pointer "FILE") gsl-permutation)
+       FILE permutation)))
+
+  (define (permutation-fprintf fileport permutation format)
+    (let* ((FILE (get-c-file 'permutation-fprintf fileport)))
+      ((foreign-safe-lambda gsl-errno "gsl_permutation_fprintf"
+         (c-pointer "FILE") gsl-permutation c-string)
+       FILE permutation format)))
+
+  (define (permutation-fscanf! fileport permutation)
+    (let* ((FILE (get-c-file 'permutation-fscanf fileport)))
+      ((foreign-safe-lambda gsl-errno "gsl_permutation_fscanf"
+         (c-pointer "FILE") gsl-permutation)
+       FILE permutation)))
+
+  ;;; Permutations in cyclic form
+  (define permutation-linear-to-canonical!
+    (foreign-safe-lambda gsl-errno "gsl_permutation_linear_to_canonical" gsl-permutation gsl-permutation))
+  (define permutation-canonical-to-linear!
+    (foreign-safe-lambda gsl-errno "gsl_permutation_linear_to_canonical" gsl-permutation gsl-permutation))
+
+  (define permutation-inversions
+    (foreign-lambda size_t "gsl_permutation_inversions" gsl-permutation))
+  (define permutation-linear-cycles
+    (foreign-lambda size_t "gsl_permutation_linear_cycles" gsl-permutation))
+  (define permutation-canonical-cycles
+    (foreign-lambda size_t "gsl_permutation_canonical_cycles" gsl-permutation)))
