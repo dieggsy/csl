1dfff7433b27b2e3e4398080bb46b98929b6d613
dieggsy <dieggsy@protonmail.com>
2019-10-06 09:50:03 -0400

    Add helper macro for permute

--- utils/syntax-utils.scm
+++ utils/syntax-utils.scm
@@ -1,5 +1,9 @@
 (import chicken.syntax)
 
+(import-for-syntax number-limits
+                   (only chicken.base symbol-append)
+                   (only chicken.string substring-index))
+
 (define-for-syntax (complex-ctype-string type)
   (case type
     ((complex)
@@ -23,3 +27,28 @@
     ((double float)
      "C_flonum")
     (else "ERROR")))
+
+(define-for-syntax sizet-array-foreign-type
+  (cond-expand (64bit 'u64vector) (else 'u32vector)))
+
+(define-for-syntax (ctype-array-foreign-type base-type)
+  (let ((prefix (cond ((or (eqv? base-type 'double)
+                           (eqv? base-type 'float))
+                       'f)
+                      ((substring-index "unsigned" (symbol->string base-type))
+                       'u)
+                      (else 's))))
+    (symbol-append
+     prefix
+     (string->symbol
+      (number->string
+       (* 8
+          (case base-type
+            ((byte unsigned_byte) char-size)
+            ((short unsigned_short) short-size)
+            ((int unsigned_int) int-size)
+            ((long unsigned_long) long-size)
+            ((double) 8)
+            ((float) 4)))))
+     'vector)))
+
